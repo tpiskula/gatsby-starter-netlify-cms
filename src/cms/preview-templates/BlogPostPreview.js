eb73e2478d09ca3fd863e2a69ae6a3ee65220023
Tobias Piskula <tobias.piskula@gmail.com>
2018-02-07 21:10:02 +0100

    log markdow

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BlogPostTemplate } from '../../templates/blog-post';
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
+const BlogPostPreview = ({ entry, widgetFor }) => 
+{
+  console.log(entry.getIn(['data', 'body']))
+  return (
   <BlogPostTemplate
     content={widgetFor('body')}
     description={entry.getIn(['data', 'description'])}
     title={entry.getIn(['data', 'title'])}
   />
-);
+)};
 
 export default BlogPostPreview;
