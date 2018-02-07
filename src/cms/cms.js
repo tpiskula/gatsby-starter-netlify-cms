e52f3fc85e140233ebfe73fb807709987103d867
Tobias Piskula <tobias.piskula@gmail.com>
2018-02-07 12:45:35 +0100

    component syntax pattern

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -17,7 +17,7 @@ CMS.registerEditorComponent({
   // Fields the user need to fill out when adding an instance of the component
   fields: [{name: 'id', label: 'Youtube Video ID', widget: 'string'}],
   // Pattern to identify a block as being an instance of this component
-  pattern: /^youtube (\S+)$/,
+  pattern: /^<Youtube id="(\S+)"><\/Youtube>/,
   // Function to extract data elements from the regexp match
   fromBlock: function(match) {
     return {
@@ -26,7 +26,7 @@ CMS.registerEditorComponent({
   },
   // Function to create a text block from an instance of this component
   toBlock: function(obj) {
-    return 'youtube ' + obj.id;
+    return '<Youtube id="'+ obj.id +'"></Youtube>';
   },
   // Preview output for this component. Can either be a string or a React component
   // (component gives better render performance)
