8f186143edacdd7667ec95442e2146d30f4efd27
Tobias Piskula <tobias.piskula@gmail.com>
2018-02-08 11:02:51 +0100

    responsive style

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -8,7 +8,23 @@ export default class Youtube extends Component {
 
   render() {
     return (
-      <iframe width="560" height="315" src={`https://www.youtube.com/embed/${this.props.id}`} frameborder="0" allowfullscreen></iframe>
+      <div style={{
+        position: 'relative',
+        padding-bottom: '56.25%',
+        padding-top: 25,
+        height: 0,
+      }}>
+        <iframe
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+          src={`https://www.youtube.com/embed/${this.props.id}`} frameborder="0" allowfullscreen>
+        </iframe>
+      </div>
     )
   }
 }
