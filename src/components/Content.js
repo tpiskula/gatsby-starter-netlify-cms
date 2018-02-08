d175e27cea9d9d17cb1b313ad87b7e42f0349dbf
Tobias Piskula <tobias.piskula@gmail.com>
2018-02-08 10:10:45 +0100

    use raw

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import rehypeReact from "rehype-react";
+import raw from 'rehype-raw';
 import Counter from "./Counter";
 import Youtube from './Youtube';
 
@@ -9,7 +10,7 @@ const renderAst = new rehypeReact({
     "interactive-counter": Counter,
     "youtube-player": Youtube,
   },
- }).Compiler;
+ }).use(raw).Compiler;
 
 export default ({ content, className }) => <div className={className}>{content}</div>;
 
